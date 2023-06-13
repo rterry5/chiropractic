9d44121f2054a37f3e32de3832711838df357b30
Rebecca Terry <rebeccaterry@Rebeccas-MacBook-Air.local>
2023-06-12 20:24:54 -0700

    adding more content pages

---
+++ domain/content.ts
@@ -0,0 +1,9 @@
+export class Content {
+  public subtitle: string;
+  public paragraph: string;
+
+  constructor(subtitle: string, paragraph: string) {
+    this.subtitle = subtitle;
+    this.paragraph = paragraph;
+  }
+}
